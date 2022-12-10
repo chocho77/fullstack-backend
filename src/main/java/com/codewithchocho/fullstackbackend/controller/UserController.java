475ae905c5bcba2281d6a69f860abd33bfed2906
chavdar <chavmom@gmail.com>
2022-12-10 17:36:17 +0200

    Changes to be committed: 	modified:   src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java

--- src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java
@@ -2,13 +2,11 @@ package com.codewithchocho.fullstackbackend.controller;
 
 import com.codewithchocho.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.codewithchocho.fullstackbackend.model.User;
 import java.util.List;
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
