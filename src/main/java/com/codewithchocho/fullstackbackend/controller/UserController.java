418861984b2f10bc011fc4a8e6ea3e08f209af5e
chavdar <chavmom@gmail.com>
2022-12-12 16:50:32 +0200

    Changes to be committed: 	modified:   src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java

--- src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewithchocho/fullstackbackend/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
     User updateUser(@RequestBody User newUser, @PathVariable Long id){
         return userRepository.findById(id)
                 .map(user -> {
-                    user.setUsername(newUser.setUsername());
+                    user.setUsername(newUser.getUsername());
                     user.setName(newUser.getName());
                     user.setEmail(newUser.getEmail());
                     return userRepository.save(user);
