3414b3c699e8dd5add90843d496d56390fdc41c1
chavdar <chavmom@gmail.com>
2022-12-12 14:46:52 +0200

    Changes to be committed: 	modified:   src/main/java/com/codewithchocho/fullstackbackend/exception/UserNotFoundAdvice.java

--- src/main/java/com/codewithchocho/fullstackbackend/exception/UserNotFoundAdvice.java
+++ src/main/java/com/codewithchocho/fullstackbackend/exception/UserNotFoundAdvice.java
@@ -2,7 +2,13 @@ package com.codewithchocho.fullstackbackend.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import java.util.Map;
+
 @ControllerAdvice
 public class UserNotFoundAdvice {
+
+    public Map<String, String> exceptionHandler(UserNotFoundException exception){
+        
+    }
 
 }
